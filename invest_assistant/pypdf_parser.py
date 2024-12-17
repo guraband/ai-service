ffc802338095c334bb409af02b77d08309b8804f
NG <guraband@naver.com>
2024-12-18 02:04:40 +0900

    document split test

--- invest_assistant/pypdf_parser.py
+++ invest_assistant/pypdf_parser.py
@@ -1,14 +1,12 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+import os
+
+OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
 
 loader = PyPDFLoader("./resource/bok_report.pdf")
-pages = loader.load_and_split()
-
-text = pages[0].page_content
-print(text)
-print("=" * 50)
-print(f"# total pages : {len(pages)}")
-print(f"# 첫 페이지의 글자수 : {len(text)}")
 
 text_splitter = CharacterTextSplitter(
     separator='.',
@@ -17,9 +15,24 @@ text_splitter = CharacterTextSplitter(
     length_function=len
 )
 
+
+# page 단위로 split
+pages = loader.load_and_split()
+text = pages[0].page_content
+print(text)
+print("=" * 50)
+print(f"# total pages : {len(pages)}")
+print(f"# 첫 페이지의 글자수 : {len(text)}")
+
 texts = text_splitter.split_text(text)
 
 print("=" * 50)
 print(f"# 첫번째 chunk : {len(texts[0])}")
 print(texts[0])
 print("=" * 50)
+
+# document 전체를 처리
+document = loader.load()
+texts = text_splitter.split_documents(document)
+print(texts[0])
+print(len(texts))
